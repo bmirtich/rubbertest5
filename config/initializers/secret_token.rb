d663e2f53cf2acf21df1a28d692a9a9cb2dcc21c
bmirtich <bmirtich@users.noreply.github.com>
2014-07-14 02:26:17 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubbertest5::Application.config.secret_key_base = 'dd8b65428dae4e968321c24e9c5719f5fef70ddf3eca03d2166fa372737ac0423dc09abd9dbbd130bdffb2304feb9bb48de264bf1ce5890e377ced191fc96ae9'
